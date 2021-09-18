1389ebff64098bd3c58b6140df6471d637fb1f11
bobby3003 <roberto3003@riseup.net>
2021-09-19 04:53:13 +0700

    first commit

---
+++ main.rs
@@ -0,0 +1,25 @@
+// SCALAR TYPES
+// Integer types and Floating point types
+
+fn main() {
+    let a = 2; // u8
+    let b = 3; // i8
+
+    let x = 2.0; // f64
+    let y: f32 = 3.0; // f32
+}
+
+// Numeric operations
+
+fn main() {
+    // addition
+    let sum = 5 + 10;
+    // subtraction
+    let difference = 95.5 - 4.3;
+    // multiplication
+    let product = 4 * 30;
+    // division
+    let quotient = 56.7 / 32.2;
+    // remainder
+    let remainder = 43 % 5;
+}
